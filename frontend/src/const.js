69508f6837dd793e6aaed41a4806173f768824a1
Daniel Shifman <shifmandaniel@gmail.com>
2024-03-25 00:27:10 -0700

    Cloud mods

--- frontend/src/const.js
+++ frontend/src/const.js
@@ -1,1 +1,1 @@
-export const BACKEND_ADDR = 'http://bc-fsd2-code-challenge-42mpbwkwra-uw.a.run.app' //TODO: Set hosted addr
+export const BACKEND_ADDR = 'http://bc-fsd2-code-challenge-42mpbwkwra-uw.a.run.app'
